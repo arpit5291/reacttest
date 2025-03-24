3a1c7cabdb36c178cb793652a3d277c34072b83e
arpit5291 <arpit856@gmail.com>
2025-03-24 18:54:01 +0530

    task 2

--- App.js
+++ App.js
@@ -1,5 +1,32 @@
-const childe = React.createElement('h1', {}, 'Hello World!');
-const childiv = React.createElement('div', {id:'child'}, childe);
-const parent = React.createElement('div', {id:'parent'}, childiv);
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(parent);
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import "./style.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FaSearch } from "react-icons/fa";
+import { FaRegUser } from "react-icons/fa";
+const Str = () => (
+  <div className="header">
+    <ul className="child">
+      <li id="lodestone">
+        <img
+          src="https://dunkin.co.uk/media/logo/default/logo_dunkin-donuts.svg"
+          className="search-icon"
+        />
+      </li>
+      <li id="mogstation">
+        <div className="search">
+          <FaSearch className="search-icon" />
+          <input type="text" placeholder="Search" className="search-input" />
+        </div>
+      </li>
+      <li id="user">
+        <div className="header-right right">
+          <FaRegUser className="user-icon" />
+        </div>
+      </li>
+    </ul>
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Str />);
--- t.php
+++
@@ -1,8 +0,0 @@
-<?php
-$_COOKIE = 'test';
-$writer = new \Zend\Log\Writer\Stream(BP . '/var/log/Logdone.log');
-logger = new \Zend\Log\Logger();
-logger->addWriter(writer);
-logger->info('');
-logger->info(print_r($, true));
-?>
